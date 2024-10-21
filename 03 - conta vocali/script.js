95b08e73be557b48cbb522506504ac81fc3e6872
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-21 18:52:13 +0200

    add

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,18 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
-
+const vocalNumber= string=>{
+    let count=0;
+    for(let i=0;i<string.length;i++){
+        if(string.charAt(i).toLowerCase()==="a"||string.charAt(i).toLowerCase()==="e"||string.charAt(i).toLowerCase()==="i"||string.charAt(i).toLowerCase()==="o"||string.charAt(i).toLowerCase()==="u"){
+            count++;
+        }
+    
+    }
+    return count;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vocalNumber(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
