9f42ba653317881cd3ebd2704aa1eb67e8424d05
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-21 18:41:26 +0200

    add

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const stringStartWith=(array,letter)=>{
+    const newArray=[];
+    for(let i=0;i<array.length;i++){
+        if(array[i].charAt(0)===letter){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(stringStartWith(names,"A"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
