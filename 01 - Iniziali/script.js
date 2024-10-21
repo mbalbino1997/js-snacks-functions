280339df55dd9a8b9b1d13fa2baad6214a0ccbdc
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-21 18:31:22 +0200

    add

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,11 +4,10 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-const arrayFirstLetter = (array) =>{
+const arrayFirstLetter = array =>{
     const newArray=[];
     for(let i=0;i<array.length;i++){
         newArray[i]=array[i].at(0);
-
     }
     return newArray;
 }
@@ -18,3 +17,22 @@ console.log(arrayFirstLetter(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
+//bonus: in ogni elemento di names ci sono più parole e voglio ottenere un nuovo array di array contenente le iniziali di ogni parola 
+//Risultato atteso: [["A","L","M"],["L","G","N"],["M","F"],["A","S"],["G"],["A","P"]]
+const names2 = ["Anna Lucia Marta", "Luca Giovanni Nicola", "Marco Federico", "Adele Simona", "Giovanni", "Alessandra Paola"];
+const arrayFirstEachLetter = array =>{
+    const newArray=[];
+    for(let i=0;i<array.length;i++){
+        const innerArray=array[i].split(" ");
+        const initials=[];
+        for(let j=0;j<innerArray.length;j++){
+            initials.push(innerArray[j].charAt(0));
+        }
+        newArray.push(initials);
+    }
+    return newArray;
+}
+console.log(arrayFirstEachLetter(names2))
